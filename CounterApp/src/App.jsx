d5afdcc938a7fe67cf8a050fe8ff7af1759530a3
Lee hwan seuk <sioo1014@naver.com>
2025-02-20 16:15:11 +0900

    feat(CounterApp):카운터앱 화면 UI 구현

--- CounterApp/src/App.jsx
+++ CounterApp/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 
+import Viewer from './components/Viewer'
+import Controller from './components/Controller'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
+    <div className='App'>
+     <h1>Simple Counter</h1>
+     <section>
+      <Viewer/>
+     </section> 
+     <section>
+      <Controller/>
+     </section>
+    </div>
   )
 }
 
