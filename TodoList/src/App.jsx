a58c13f77456550bd9a949005f52a39da9d559af
Lee hwan seuk <sioo1014@naver.com>
2025-02-20 19:10:01 +0900

    feat(TodoList): 투두 리스트 UI 구현

--- TodoList/src/App.jsx
+++ TodoList/src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
 
+import Header from './components/Header';
+import Editor from './components/Editor';
+import List from './components/List';
+import TodoItem from './components/TodoItem';
+
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div className='App'>
+      <section>
+        <Header/>
+      </section>
+      <section>
+        <Editor/>
+      </section>
+      <section>
+        <List/>
+      </section>
+      
+
+    </div>
   )
 }
 
---
+++ TodoList/src/components/List.jsx
@@ -0,0 +1,19 @@
+import './List.css'
+import TodoItem from './TodoItem';
+
+const List = ()=>{
+
+    return(
+        <div className='List'>
+            <h3>Todo List🌱</h3>
+            <input placeholder='검색어를 입력하세요...'/>
+            <div className='todos_wrapper'>
+              <TodoItem/>
+              <TodoItem/>
+              <TodoItem/>
+            </div>
+        </div>
+    )
+};
+
+export default List;
